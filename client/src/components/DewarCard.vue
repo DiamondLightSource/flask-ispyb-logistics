494500e8e747bc09336d2c3a4fdf5cdcb775c216
Neil A Smith <neil.smith@diamond.ac.uk>
2019-10-03 16:46:58 +0100

    Added card component for dewar pages

---
+++ client/src/components/DewarCard.vue
@@ -0,0 +1,30 @@
+<!--
+Card displaying dewar information stored in a specific rack location
+
+Emits an event 'clear-location' which should be handled by the parent component
+-->
+<template>
+    <div class="border rounded shadow h-full p-4 cursor-pointer" v-bind:class="{'text-danger': dewar.needsLN2 && dewar.status !== 'dispatch-requested'}">
+        <span class="font-bold">{{rack}}: </span>
+        <span v-if="dewar.barcode" class="font-bold">{{dewar.barcode}}</span>
+        <span v-else class="">empty</span>
+        <!-- Tags -->
+        <div class="flex flex-wrap">
+            <span v-if="dewar.arrivalDate" class="text-xs text-white bg-info py-1 px-2 ">{{dewar.arrivalDate.split(' ').slice(0,4).join(" ")}}</span>
+            <span v-if="dewar.facilityCode" class="text-xs text-white bg-gray-900 py-1 px-2">{{dewar.facilityCode}}</span>
+            <span v-if="dewar.status == 'dispatch-requested'" class="text-xs bg-warning py-1 px-2 ">{{dewar.status}}</span>
+            <span v-else-if="dewar.needsLN2" class="text-xs text-white bg-danger py-1 px-2 ">needs-refill</span>  
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'DewarCard',
+    props: {
+        dewar: Object,
+        rack: String
+    },
+}
+</script>
