7a99e88cfaa0ccc2b504f7e94d368505f5d19ce7
Neil A Smith <neil.smith@diamond.ac.uk>
2019-11-08 10:42:00 +0000

    Updated test for zone4 changes

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -6,6 +6,7 @@ import logging.handlers
 import unittest
 from flask import Flask
 from flask_testing import TestCase
+import json
 
 # Run this from the parent dir
 # Run with python -m tests/test_controller
@@ -15,14 +16,17 @@ from ispyb_api import db
 from ispyb_api import models
 from ispyb_api import controller
 from dewars.zone6 import rack_locations
+from dewars.zone4 import rack_locations as zone4_locations
 
 class MyTest(TestCase):
-    def create_app(sekf):
+    def create_app(self):
         app = Flask(__name__)
         app.config['TESTING'] = True
 
         os.environ['ISPYB_CONFIG_FILE'] = 'tests/test.cfg'
         os.environ['ISPYB_CONFIG_SECTION'] = 'ispyb_dev'
+
+        self.logger = logging.getLogger('ispyb-logistics')
 
         ispyb_api.init_app(app)
 
@@ -38,15 +42,26 @@ class MyTest(TestCase):
         db.session.remove()
 
     def test_get_dewar_by_facilitycode(self):
-        print("Test Get Dewars for facilitycode {}".format(self.facilitycode))
+        self.logger.debug("Test Get Dewars for facilitycode {}".format(self.facilitycode))
         result = controller.get_dewar_by_facilitycode(self.facilitycode)
-        print(result)
+        self.logger.debug(result)
         self.assertIsNotNone(result)
 
     def test_get_dewars_by_locations(self):
-        print("Test Get Dewars for locations {}".format(','.join(self.locations)))
+        self.logger.debug("Test Get Dewars for locations {}".format(','.join(self.locations)))
         result = controller.find_dewars_by_location(self.locations)
-        print(result)
+        self.assertIsNotNone(result)
+
+    def test_get_dewar_history_by_locations(self):
+        self.logger.debug("Test Get Dewar History for locations {}".format(','.join(zone4_locations)))
+        result = controller.find_dewar_history_for_locations(zone4_locations, max_entries=-1, match_locations = False)
+        self.logger.debug(json.dumps(result, indent=4, sort_keys=True))
+        self.assertIsNotNone(result)
+
+    def test_get_dewar_history_by_locations(self):
+        self.logger.debug("Test Get Dewar History for locations {}".format(','.join(zone4_locations)))
+        result = controller.find_recent_storage_history(zone4_locations)
+        self.logger.debug(json.dumps(result, indent=4, sort_keys=True))
         self.assertIsNotNone(result)
 
 
