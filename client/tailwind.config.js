b6c6d2e2562b94c1a5c8f768b2eb489c77ce9d3a
Mark Williams <mark.williams@diamond.ac.uk>
2023-11-27 15:46:19 +0000

    Configure font size line heights

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
           display: ['Gilroy', 'sans-serif'],
           body: ['Cantarell', 'sans-serif'],
         },
+        fontSize: {
+          xs: '0.75rem',
+          sm: '0.875rem',
+          base: '1rem',
+          lg: '1.125rem',
+          xl: '1.25rem',
+          '2xl': '1.5rem',
+          '3xl': '1.875rem',
+          '4xl': '2.25rem',
+          '5xl': '3rem',
+          '6xl': '4rem',
+        },
         borderWidth: {
           DEFAULT: '1px',
           '0': '0',
