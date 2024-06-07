de1d52f7982ef7fa963811cf0ae66e5a6ac562ab
Kate Smith <katemarylouisesmith@gmail.com>
2024-06-07 11:24:22 +0100

    bug fix sls section and add trolley/tray prefix

--- api/dewars/zone4.py
+++ api/dewars/zone4.py
@@ -1,7 +1,7 @@
 #
 # Configuration for the Zone4 Storage Area
 #
-rack_prefix = 'TRAY'
+rack_prefix = 'TRAY-'
 
 # 16 normal trays with A-F
 num_trays = 16
@@ -14,12 +14,12 @@ trays = ["{}".format(i) for i in range(1,num_trays+1)]
 
 # Build the list of TRAY-01A, TRAY-01B, TRAY-01C...
 for tray in trays:
-  rack_locations.extend(''.join(['TRAY-', tray, pos]) for pos in positions)
+  rack_locations.extend(''.join([rack_prefix, tray, pos]) for pos in positions)
 
 # Extend with Tray 17 A-P
 positions.extend('G H I J K L M N O P'.split())
 
-rack_locations.extend(''.join(['TRAY-17', pos]) for pos in positions)
+rack_locations.extend(''.join([rack_prefix+str(num_trays + 1), pos]) for pos in positions)
 
 beamline_locations = ['I03',
                       'I04',
