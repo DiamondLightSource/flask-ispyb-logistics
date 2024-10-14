1fa19953090877af6569c6731af77c51c59749b2
Mark Williams <mark.williams@diamond.ac.uk>
2024-10-14 17:10:16 +0100

    Allow other locations to be scanned on i19 page

--- api/dewars/i19.py
+++ api/dewars/i19.py
@@ -5,4 +5,10 @@
 rack_locations = ['I19-STORAGE']
 max_per_location = 8
 suffixes = ["-{}".format(i) for i in range(1,max_per_location+1)]
-beamline_locations = []
+beamline_locations = ['I19',
+                      'I19-STORAGE',
+                      'USER-COLLECTION',
+                      'STORES-OUT',
+                      'I19-TO-STORES',
+                      'LN2TOPUP',
+                      ]
