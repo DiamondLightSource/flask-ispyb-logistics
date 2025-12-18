9d74301f37a80275c0e4ed15a9950570b1f1c864
Mark Williams <mark.williams@diamond.ac.uk>
2025-12-18 12:58:50 +0000

    Allow strings and arrays of strings

--- client/src/components/DewarReportDialog.vue
+++ client/src/components/DewarReportDialog.vue
@@ -93,7 +93,7 @@ export default {
             type: String,
         },
         dewarContainers: {
-            type: String,
+            type: [String, Array],
         },
         visit: {
             type: String,
