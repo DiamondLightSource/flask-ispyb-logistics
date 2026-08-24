2c632d00a71ebd5f6aeac905c57bfb0e72d42ead
Mark Williams <mark.williams@diamond.ac.uk>
2026-08-24 14:21:36 +0100

    Get expected AWB from shipping service

--- api/ispyb_api/controller.py
+++ api/ispyb_api/controller.py
@@ -16,7 +16,6 @@ from . import send_email
 from .models import Dewar, DewarTransportHistory, LabContact, Laboratory, Shipping, Proposal, Person, BLSession, Container, ContainerQueue
 
 from ..dewars import ebic
-from ..stores import shipping_service
 
 email_domain = os.environ.get('EMAIL_DOMAIN', '@diamond.ac.uk')
 rest_api = True if os.environ.get("REST_API", "0") == "1" else False
